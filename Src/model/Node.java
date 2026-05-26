a135ea68cfcdcf7e27173526ec6facf01c297ce1
shirotran123-cloud <shirotran123@gmail.com>
2026-05-26 15:46:00 +0700

    Add files via upload

---
+++ Src/model/Node.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * Node bọc Song dùng cho Circular Doubly Linked List.
+ * Mỗi node chứa một bài hát và hai con trỏ: next và prev.
+ */
+public class Node {
+    public Song song;
+    public Node next;
+    public Node prev;
+
+    public Node(Song song) {
+        this.song = song;
+        this.next = null;
+        this.prev = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{song=" + (song != null ? song.getTitle() : "null") + "}";
+    }
+}
